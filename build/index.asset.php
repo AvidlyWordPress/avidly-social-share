43011547f5053e07f0ffc7287e9de5183aa7ec8f
Veera Peltomäki <veera.peltomaki@avidlyagency.com>
2023-08-08 17:21:14 +0300

    Build index.js assets with updated scripts (not used in frontend)

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'a3868f8bdeaa26a499b1c010601d0de0');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '0ea9c49572a0a3d7a2c9');
